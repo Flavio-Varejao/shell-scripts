e1f5587ede7f915fa47c370b42956193a516c4f8
Flavio-Varejao <gutosoni@gmail.com>
2020-03-30 09:37:58 -0300

    Adicionado novos scripts

---
+++ Backup com rsync/dialog version/rsync-backup-dv1.0.sh
@@ -0,0 +1,119 @@
+#!/usr/bin/env bash
+#
+# rsync-backup-dv1.0.sh - Backup com a ferramenta rsync
+#
+# Site:     
+# Autor:      Flávio Varejão
+# Manutenção: Flávio Varejão
+# --------------------------------------------------------------------------------------------------------------------------- #
+# Este script faz o backup de arquivos locais ou remotos utilizando a ferramenta rsync 
+# Essa versão utiliza caixas de diálogo do DIALOG. Leia as instruções a seguir.
+#
+# Dê permissão de execução (primeiro acesso):
+#   $ chmod +x rsync-backup-dv1.0.sh
+#
+# Exemplos:
+#   $ ./rsync-backup-dv1.0.sh
+#   Neste exemplo o script vai perguntar se você deseja fazer um backup.
+#
+# Na execução do script é possível escolher o diretório de origem e de destino dos arquivos.
+# No término do backup é gerado um arquivo log com informações do backup 
+# (data, hora, arquivos copiados, etc).
+#
+# Opções incluídas no comando rsync:
+# -u  Envia somente novos arquivos ou modificados;
+# -a  Modo de arquivamento (inclui diretórios e mantém os metadados);
+# -v  Modo verboso (visualiza o processo);
+# -h  Números compreensíveis para humanos;
+# -P  Exibe os tempos de transferência, nomes dos arquivos e diretórios sincronizados.
+# --progress - Mostra o progresso durante a transferência
+#
+# Para incluir novas opções (rsync --help) altere os comandos do rsync na seção de FUNÇÕES.   
+# --------------------------------------------------------------------------------------------------------------------------- #
+# Histórico:
+#   Versão 1.0, Flávio:
+#     25/03/2020
+#       - Início do programa
+#       - Adicionado variáveis, testes, funções e execução
+#     28/03/2020         
+#       - Adicionado barras de progresso
+# --------------------------------------------------------------------------------------------------------------------------- #
+# Testado em:
+#   bash 4.4.20 
+# --------------------------------------------------------------------------------------------------------------------------- #
+# ------------------------------------------------ VARIÁVEIS------------------------------------------------- #
+LOG="$(date +%m%Y)"
+
+ARQUIVO_LOG="bkp-$LOG.log"
+
+MENSAGEM_LOG="#$(date "+%A, %d %B %Y")#" 
+# --------------------------------------------------------------------------------------------------------------------------- #
+# ------------------------------------------------- TESTES -------------------------------------------------- # 
+[ ! -x "$(which dialog)" ] && sudo apt install dialog -y 1> /dev/null 2>&1 # dialog instalado?
+[ ! -x "$(which rsync)" ] && sudo apt install rsync -y # rsync instalado?
+# --------------------------------------------------------------------------------------------------------------------------- #
+# ------------------------------------------------- FUNÇÕES ------------------------------------------------- #
+Backup_local () {
+  dialog --infobox 'Iniciando Backup...' 3 25; sleep 1
+  echo "$MENSAGEM_LOG" >> "$ARQUIVO_LOG"
+  rsync -avh --progress "$dir_origem" "$dir_destino" --log-file="$ARQUIVO_LOG" \
+  | perl -lane 'BEGIN { $/ = "\r"; $|++ } $F[1] =~ /(\d+)%$/ && print $1' \
+  | dialog --gauge 'Aguarde... Copiando Arquivos' 8 70 0
+  dialog --msgbox 'Backup concluído com sucesso!' 6 35
+  dialog --title 'Log de Backup' --textbox "$ARQUIVO_LOG" 0 0
+  clear
+  exit
+}
+Backup_remoto () {
+  dialog --infobox 'Iniciando Backup...' 3 25; sleep 1
+  echo "$MENSAGEM_LOG" >> "$ARQUIVO_LOG"
+  sudo rsync -avhP --progress "$dir_origem" "$dir_destino" --log-file="$ARQUIVO_LOG" \
+  | perl -lane 'BEGIN { $/ = "\r"; $|++ } $F[1] =~ /(\d+)%$/ && print $1' \
+  | dialog --gauge 'Aguarde... Copiando Arquivos' 8 70 0
+  dialog --msgbox 'Backup concluído com sucesso!' 6 35
+  dialog --title 'Log de Backup' --textbox "$ARQUIVO_LOG" 0 0
+  clear
+  exit
+}
+# ---------------------------------------------------------------------------------------------------------------------------- #
+# ------------------------------------------------- EXECUÇÃO ------------------------------------------------ #
+dialog --title 'Backup com rsync' \
+--yesno 'Deseja fazer um Backup?' 6 28
+
+[ $? -eq 0 ] && {
+  menu=$(dialog --title 'Backup com rsync' \
+  --menu 'Selecione o tipo de Backup' 10 32 2 \
+  1 'Backup local' \
+  2 'Backup remoto' --stdout
+  )
+  case "$menu" in
+    1) 
+      dir_origem=$(dialog --title 'Selecione o arquivo ou diretório' \
+      --fselect "$HOME"/ 14 80 --stdout)
+      [ $? -eq 0 ] && {
+        dir_destino=$(dialog --title 'Selecione o diretório em que será salvo' \
+        --dselect "$HOME"/ 14 50 --stdout)
+        [ $? -eq 0 ] && {
+          Backup_local
+        }
+        clear && exit 0
+      }  
+      clear && exit 0
+    ;;
+    2)
+      dir_origem=$(dialog --title 'Selecione o arquivo ou diretório' \
+      --fselect "$HOME"/ 14 80 --stdout)
+      [ $? -eq 0 ] && {
+        dir_destino=$(dialog --title 'Selecione o diretório em que será salvo' \
+        --dselect "$HOME"/ 14 50 --stdout)
+        [ $? -eq 0 ] && {
+          Backup_remoto
+        }
+        clear && exit 0
+      }  
+      clear && exit 0
+    ;;
+  esac
+}
+clear && exit
+# ------------------------------------------------------------------------------------------------------------------------------ #
---
+++ Backup com rsync/normal version/rsync-backup.sh
@@ -0,0 +1,118 @@
+#!/usr/bin/env bash
+#
+# rsync-backup.sh - Backup com a ferramenta rsync
+#
+# Site:     
+# Autor:      Flávio Varejão
+# Manutenção: Flávio Varejão
+# --------------------------------------------------------------------------------------------------------------------------- #
+# Este script faz o backup de arquivos locais ou remotos utilizando a ferramenta rsync.
+# Leia as instruções a seguir.
+#
+# Dê permissão de execução (primeiro acesso):
+#   $ chmod +x rsync-backup.sh
+#
+# Como usar:
+#   $ ./rsync-backup.sh -l
+#   Neste exemplo o script vai executar um backup local.
+#
+# Na execução do script é possível escolher o diretório de origem e de destino dos arquivos. 
+# No término do backup é gerado um arquivo de log com informações do backup 
+# (data, hora, arquivos copiados, etc).
+#
+# Opções incluídas ou sugeridas para o comando rsync:
+# -u - Envia somente novos arquivos ou modificados;
+# -a - Modo de arquivamento (inclui diretórios e mantém os metadados);
+# -v - Modo verboso (mostra o processo);
+# -h - Números compreensíveis para humanos;
+# -P - Exibe os tempos de transferência, nomes dos arquivos e diretórios sincronizados.
+# --progress - Mostra o progresso durante a transferência
+#
+# Para incluir novas opções veja em 'rsync --help' altere o comando rsync  na seção de FUNÇÕES (linha 60 e 73).
+# --------------------------------------------------------------------------------------------------------------------------- #
+# Histórico:
+#   Versão 1.0, Flávio:
+#     17/03/2020
+#       - Início do programa
+#       - Adicionado variáveis, testes, funções e execuçãoa
+#     28/03/2020         
+#       - Adicionado tratamento de erros (função Verifica_status)          
+# --------------------------------------------------------------------------------------------------------------------------- #
+# Testado em:
+#   bash 4.4.20 
+# --------------------------------------------------------------------------------------------------------------------------- #
+# ------------------------------------------------ VARIÁVEIS------------------------------------------------- #
+VERDE="\033[32;1m"
+AMARELO="\033[33;1m"
+VERMELHO="\033[31;1m"
+
+LOG="$(date +%m%Y)"
+
+ARQUIVO_LOG="bkp-$LOG.log"
+
+MENU="
+  $0 [-OPÇÃO]
+    
+    -l  Backup local
+    -r  Backup remoto
+"
+MENSAGEM_LOG="#$(date "+%A, %d %B %Y")#" 
+# --------------------------------------------------------------------------------------------------------------------------- #
+# ------------------------------------------------- TESTES -------------------------------------------------- # 
+[ ! -x "$(which rsync)" ] && sudo apt install rsync -y # rsync instalado?
+# --------------------------------------------------------------------------------------------------------------------------- #
+# ------------------------------------------------- FUNÇÕES ------------------------------------------------- #
+Backup_local () {
+  clear && echo -e "${AMARELO}Backup local \n" && tput sgr0
+  echo "$MENSAGEM_LOG" >> "$ARQUIVO_LOG"
+  rsync -avh --progress "$dir_origem" "$dir_destino" --log-file="$ARQUIVO_LOG" # Alterar as opções se necessário
+  Verifica_status
+}
+Backup_remoto () {
+  clear && echo -e "${AMARELO}Backup remoto \n" && tput sgr0
+  echo "$MENSAGEM_LOG" >> "$ARQUIVO_LOG"
+  sudo rsync -uavhP --progress "$dir_origem" "$dir_destino" --log-file="$ARQUIVO_LOG" # Alterar as opções se necessário
+  Verifica_status
+}
+Verifica_status () {
+  tail -1 "$ARQUIVO_LOG" | grep "rsync error" 
+  if [ ! $? -eq 0 ]; then
+    echo -e "\n${VERDE}Backup concluído com sucesso. \n" && tput sgr0 && exit 0
+  else
+    echo -e "\n${VERMELHO}Backup concluído com erros. Erros mais comuns: \n
+    - Nome do arquivo e/ou diretório incorretos
+    - Arquivos ou diretório inexistentes
+    - Diretório está vazio 
+    - Nomes compostos sem * entre as palavras. Ex: /Área*de*Trabalho
+    " && tput sgr0 && exit 1
+  fi
+}   
+# --------------------------------------------------------------------------------------------------------------------------- #
+# ------------------------------------------------- EXECUÇÃO ------------------------------------------------ #
+echo -e "\n Backup com rsync \n $MENU" 
+while test -n "$1"; do
+  case "$1" in
+    -l)
+        clear && echo -e "${AMARELO}Backup local \n" && tput sgr0
+        read -rp "Informe a origem: " dir_origem                          #Exemplo: /home/user/nome*composto
+        echo -e "\nOs arquivos serão salvos em /home/$USER/Backup/ \n"
+        echo "Deseja manter esse diretório de destino? [s/n] "
+        read -rn1 resposta && echo ""
+        case "$resposta" in
+          S | s) dir_destino="/home/$USER/Backup/" && Backup_local ;; 
+          N | n) read -pr "Digite o diretório de destino: " dir_destino && Backup_local ;; 
+              *) echo "Processo abortado." && exit 1 ;;
+        esac
+    ;;
+    -r) 
+        clear && echo -e "${AMARELO}Backup remoto \n" && tput sgr0
+        read -rp "Digite a origem (servidor ou máquina local): " dir_origem && echo ""  
+        read -rp "Digite o destino (máquina local ou servidor): " dir_destino && echo ""
+        Backup_remoto
+    ;;
+     *) echo -e "${VERMELHO}Opção inválida. Digite $0 [-OPÇÃO] \n" && tput sgr0
+        exit 1
+    ;;
+  esac
+done
+# --------------------------------------------------------------------------------------------------------------------------- #
